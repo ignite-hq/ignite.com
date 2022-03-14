66e6b4e9e67342cd8a00de184810a5f199716387
marinhoarthur <arthurtutuca@gmail.com>
2022-03-14 15:13:53 -0300

    n

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,21 +9,27 @@ import { useTendermintSpnLaunchModule } from 'tendermint-spn-vue'
 import { inject, watch, Ref } from 'vue'
 
 export default {
-  setup() {
+  async setup() {
     let $ignt = inject<Ref<Ignite>>('ignt')
 
-    //watch
-    watch(
-      () => $ignt.value,
-      async () => {
-        if ($ignt.value) {
-          let { $s, queryChainAll } = useTendermintSpnLaunchModule({
-            $ignt: $ignt.value
-          })
-        }
-      },
-      { immediate: true }
-    )
+    let { queryChainAll, sendMsgRequestAddAccount } =
+      useTendermintSpnLaunchModule({
+        $ignt: $ignt.value
+      })
+
+    console.log(await queryChainAll())
+
+    // console.log(
+    //   'add acc',
+    //   sendMsgRequestAddAccount({
+    //     value: {
+    //       address: 'spn1l9wuy2w80v3ntc38ede0zdn7vn0lhslfutqnka',
+    //       coins: [{ amount: '99999', denom: 'spn' }],
+    //       creator: 'spn1l9wuy2w80v3ntc38ede0zdn7vn0lhslfutqnka',
+    //       launchID: 1
+    //     }
+    //   })
+    // )
   }
 }
 </script>
