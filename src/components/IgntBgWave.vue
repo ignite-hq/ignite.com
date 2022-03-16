23eb82ca17e114af0ce59851687c7873c5a53d35
darika <ppartesia@gmail.com>
2022-03-17 02:19:19 +0400

    static project card

---
+++ src/components/IgntBgWave.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wave" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IgntBgWave',
+})
+</script>
+
+<style scoped lang="scss">
+.wave,
+.wave::before,
+.wave::after {
+  content: '';
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  width: 500px;
+  height: 500px;
+  margin-left: -300px;
+  transform-origin: 50% 50%;
+  border-radius: 38% 42%;
+  background-color: rgba(255, 255, 255, 0.05);
+  animation: spin 16s infinite linear;
+}
+
+.wave::before {
+  width: 105%;
+  height: 95%;
+  margin-top: -300px;
+  transform-origin: 49% 51%;
+  border-radius: 40% 38%;
+  background-color: rgba(255,255,255,0.1);
+  animation: spin 13s infinite linear;
+}
+
+.wave::after {
+  width: 102%;
+  height: 98%;
+  margin-top: -300px;
+  transform-origin: 51% 49%;
+  border-radius: 48% 42%;
+  background-color: rgba(255,255,255,0.16);
+  animation: spin 10s infinite linear;
+}
+
+@keyframes spin {
+  100% { transform: rotate(360deg); }
+}
+</style>
---
+++ src/components/IgntProgressBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="ignt-progress__container">
+    <div v-if="logo" class="logo mr-3" />
+    <div class="ignt-progress" :class="inverse && '_white'">
+      <div
+        v-for="item in items"
+        :key="item.name"
+        class="ignt-progress__inner text-center"
+        :class="item.split && '_no-radius'"
+        :style="{ 'width': item.value + '%', 'background-color': item.bgColor, 'color': item.color }"
+      >
+        <span class="ignt-label xs">
+          <b>{{ item.value }}%</b>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IgntProgressBar',
+
+  props: {
+    inverse: {
+      type: Boolean
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    logo: {
+      type: String
+    },
+  },
+
+  setup(props) {
+    const { items, logo } = props;
+
+    return {
+      items,
+      logo
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.logo {
+  width: var(--spacing-6);
+  height: var(--spacing-6);
+  flex-shrink: 0;
+  border-radius: 100%;
+  background-color: var(--gray-50);
+  box-shadow: inset 0px 0px px2rem(4) var(--black-660);
+}
+
+.ignt-progress {
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  width: 100%;
+  border-radius: px2rem(100);
+  background: var(--gray-50);
+  font-size: 0;
+  &._white {
+    background-color: var(--white);
+  }
+
+  &__container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__inner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: px2rem(20);
+    border-radius: 0px px2rem px2rem 0px;
+    &._no-radius {
+      border-radius: 0;
+    }
+  }
+}
+</style>
