d5caa2dbfe389fa6fcd699fa9796bc448affd091
JoseRFelix <jfelx@outlook.com>
2022-03-24 01:40:56 -0400

    feat: add explore page pagination

--- src/composables/useLauchChains.ts
+++ src/composables/useLauchChains.ts
@@ -1,20 +1,35 @@
 import { useTendermintSpnLaunchModule } from 'tendermint-spn-vue'
 import { computed } from 'vue'
-import { useQuery } from 'vue-query'
+import { useInfiniteQuery } from 'vue-query'
 
 import useInjectedIgnite from './useInjectedIgnite'
+
+const CHAINS_PER_PAGE = '20'
 
 export default function useLaunchChains() {
   const { igniteClient } = useInjectedIgnite()
   const { queryChainAll } = useTendermintSpnLaunchModule({
     $ignt: igniteClient.value
   })
-  const { data, ...other } = useQuery('launch-chains', () =>
-    queryChainAll().then((r) => r.data)
+
+  const { data, ...other } = useInfiniteQuery(
+    'launch-chains',
+    ({ pageParam }) => {
+      return queryChainAll({
+        'pagination.limit': CHAINS_PER_PAGE,
+        'pagination.key': pageParam
+      }).then((r) => r.data)
+    },
+    {
+      getNextPageParam: (lastPage) => {
+        // @ts-ignore
+        return lastPage.pagination?.next_key
+      }
+    }
   )
 
   const allChains = computed(() => {
-    return data.value?.chain
+    return data.value
   })
 
   return { allChains, ...other }
