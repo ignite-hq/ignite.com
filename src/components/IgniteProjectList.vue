42639e8291346180de3cca91e8dec166d5a4531e
JoseRFelix <jfelx@outlook.com>
2022-03-24 01:40:56 -0400

    feat: add explore page pagination

--- src/components/IgniteProjectList.vue
+++ src/components/IgniteProjectList.vue
@@ -1,50 +1,108 @@
 <template>
   <div class="project-list">
-    <MasonryWall
-      :items="loading ? skeletons : projects"
-      :column-width="columnWidth"
-      :gap="gap"
-      :rtl="rtl"
-    >
-      <template #default="{ item: project }">
-        <IgniteProjectCard :project="project" :loading="loading" />
-      </template>
-    </MasonryWall>
+    <div class="masonry">
+      <MasonryWall
+        :items="chains"
+        :column-width="columnWidth"
+        :gap="gap"
+        :rtl="rtl"
+      >
+        <template #default="{ item: project }">
+          <IgniteProjectCard :project="project" :loading="project?.loading" />
+        </template>
+      </MasonryWall>
+    </div>
+
+    <div v-if="hasNextPage && !isFetchingNextPage" class="blur-box">
+      <SpButton class="view-more-btn" @click="() => fetchNextPage()"
+        >View More</SpButton
+      >
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'IgntProjectList'
+  name: 'IgniteProjectList'
 }
 </script>
 
 <script lang="ts" setup>
+import SpButton from '@starport/vue/src/components/SpButton/SpButton.vue'
 import MasonryWall from '@yeger/vue-masonry-wall'
-import { LaunchChain } from 'tendermint-spn-ts-client/tendermint.spn.launch/rest'
-import { computed, PropType } from 'vue'
+import {
+  LaunchChain,
+  LaunchQueryAllChainResponse
+} from 'tendermint-spn-ts-client/tendermint.spn.launch/rest'
+import { computed } from 'vue'
 
+import useLaunchChains from '../composables/useLauchChains'
 import IgniteProjectCard from './IgniteProjectCard/index.vue'
 
-const props = defineProps({
-  loading: {
-    type: Boolean,
-    default: true
-  },
-  chains: {
-    type: Array as PropType<LaunchChain[]>,
-    default: () => []
-  }
-})
-
+// variables
 const columnWidth = 330
 const gap = 32
 const rtl = false
-const skeletons = new Array(6).fill(undefined)
+const skeletons = new Array(6).fill({ loading: true })
 
-const projects = computed(() => {
-  return props.chains.filter(({ hasCampaign }) => hasCampaign)
+// composables
+const { isLoading, allChains, fetchNextPage, hasNextPage, isFetchingNextPage } =
+  useLaunchChains()
+
+// methods
+function filterCampaignlessChains(chains: LaunchChain[] = []): LaunchChain[] {
+  return chains.filter(({ hasCampaign }) => hasCampaign)
+}
+
+function mergePages(pages: LaunchQueryAllChainResponse[] = []): LaunchChain[] {
+  return pages.reduce(
+    (acc, page) => [...acc, ...(page?.chain ?? [])],
+    [] as LaunchChain[]
+  )
+}
+
+// computed
+const chains = computed<LaunchChain[]>(() => {
+  if (isLoading.value) {
+    return skeletons
+  }
+
+  if (isFetchingNextPage.value) {
+    return [...mergePages(allChains.value?.pages), ...skeletons]
+  }
+
+  return filterCampaignlessChains(mergePages(allChains.value?.pages))
 })
 </script>
 
-<style scoped lang="postcss"></style>
+<style scoped lang="postcss">
+.project-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-bottom: 32px;
+}
+
+.masonry {
+  align-self: stretch;
+}
+
+.blur-box {
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  background-color: #fff;
+  height: 6rem;
+  box-shadow: 0 0 2000px 200px #fff;
+
+  margin-top: -8rem;
+  padding: 1rem 0;
+}
+
+.view-more-btn {
+  background-color: var(--primary);
+  border-color: var(--primary);
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="container">
-    <IgniteProjectList :loading="isLoading" :chains="allChains" />
+    <IgniteProjectList />
   </div>
 </template>
 
 <script lang="ts" setup>
 import IgniteProjectList from '../components/IgniteProjectList.vue'
-import useLaunchChains from '../composables/useLauchChains'
-
-const { isLoading, allChains } = useLaunchChains()
 </script>
