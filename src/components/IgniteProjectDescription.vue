7fa015ba30acc3527bbefaffc7c6f96151aaab00
darika <ppartesia@gmail.com>
2022-04-01 17:27:49 +0400

    add description component

---
+++ src/components/IgniteProjectDescription.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="container-full container px-5 sm:px-5.5 l:px-7">
+    <div
+      class="grid grid-cols-1 l:grid-cols-12 gap-x-6 m:gap-x-7 project-description"
+    >
+      <h1>About the project</h1>
+      <img
+        src="https://cpmr-islands.org/wp-content/uploads/sites/4/2019/07/Happy-Test-Screen-01-825x510-600x371.png"
+      />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit est
+        metus, at fermentum quam mollis eu. Phasellus posuere augue massa. Donec
+        eleifend maximus elementum. Vestibulum ullamcorper risus ut elementum
+        molestie. Pellentesque varius porta risus interdum condimentum.
+      </p>
+      <p>
+        Sed et purus ac lectus hendrerit laoreet. Donec mollis pretium leo, id
+        commodo felis dictum in. Etiam vel mauris non odio feugiat ultrices sit
+        amet a libero. Ut et semper ipsum. Nunc tristique purus eu pellentesque
+        condimentum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit est
+        metus, at fermentum quam mollis eu. Phasellus posuere augue massa. Donec
+        eleifend maximus elementum. Vestibulum ullamcorper risus ut elementum
+        molestie. Pellentesque varius porta risus interdum condimentum. Sed et
+        purus ac lectus hendrerit laoreet. Donec mollis pretium leo, id commodo
+        felis dictum in. Etiam vel mauris non odio feugiat ultrices sit amet a
+        libero. Ut et semper ipsum. Nunc tristique purus eu pellentesque
+        condimentum. Aliquam placerat orci quis mauris bibendum, eu tristique
+        urna dapibus. Vestibulum efficitur efficitur urna, eu bibendum lorem
+        viverra in.
+      </p>
+
+      <h2>The market</h2>
+      <img
+        src="https://cpmr-islands.org/wp-content/uploads/sites/4/2019/07/Happy-Test-Screen-01-825x510-600x371.png"
+      />
+      <h3>Games in the Cosmos</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit est
+        metus, at fermentum quam mollis eu. Phasellus posuere augue massa. Donec
+        eleifend maximus elementum. Vestibulum ullamcorper risus ut elementum
+        molestie. Pellentesque varius porta risus interdum condimentum. Sed et
+        purus ac lectus hendrerit laoreet. Donec mollis pretium leo, id commodo
+        felis dictum in. Etiam vel mauris non odio feugiat ultrices sit amet a
+        libero. Ut et semper ipsum. Nunc tristique purus eu pellentesque
+        condimentum. Aliquam placerat orci quis mauris bibendum, eu tristique
+        urna dapibus. Vestibulum efficitur efficitur urna, eu bibendum lorem
+        viverra in.
+      </p>
+
+      <h2>The opportunity</h2>
+      <h3>Collectables that travel with you</h3>
+      <p>
+        Etiam in ligula orci. Integer auctor varius turpis, sit amet tempus
+        risus. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Vivamus in vehicula ipsum. Praesent eu
+        vestibulum ligula. Sed cursus elementum urna, ac volutpat erat. Nulla
+        eleifend, ex ut pulvinar aliquam, magna quam volutpat urna, eget
+        sagittis dolor dui eget erat. In et nibh a eros sodales varius nec sit
+        amet est. In ultrices sapien purus, eu feugiat est gravida vel. Praesent
+        rutrum laoreet tempor. Morbi id lectus a quam facilisis lacinia.
+        Curabitur vitae ornare elit.
+      </p>
+      <p><b>Suspendisse et tincidunt sem:</b></p>
+      <ul>
+        <li>Vivamus in vehicula ipsum</li>
+        <li>Praesent rutrum laoreet tempor</li>
+        <li>Integer auctor varius turpis, sit amet tempus risus</li>
+        <li>In ultrices sapien purus</li>
+      </ul>
+
+      <h2>Unique positioning</h2>
+      <h3>Collectables that travel with you</h3>
+      <p>
+        Fusce convallis massa eu vestibulum finibus. Ut eget interdum nunc, eget
+        pulvinar elit. Sed erat mi, egestas ut sagittis non, dignissim at risus.
+        Curabitur posuere, lacus eu efficitur lacinia, erat ligula hendrerit
+        turpis, sit amet convallis enim dui id dui. Sed congue dui feugiat
+        consequat dignissim. Vestibulum at felis a tortor maximus vehicula at at
+        libero. Quisque feugiat in quam eget mollis.
+      </p>
+      <p>
+        Morbi eu ex lectus. Curabitur purus mauris, finibus a felis in, vehicula
+        gravida est. Nullam eu tortor id velit malesuada facilisis eu at mauris.
+        Integer rhoncus interdum tortor, sed malesuada tortor luctus feugiat.
+        Quisque cursus elit eros, sagittis egestas est faucibus nec.
+      </p>
+
+      <h2>Business model</h2>
+      <h3>An NFT marketplace</h3>
+      <p>
+        Nullam velit ipsum, tempor quis euismod eget, maximus non leo. Curabitur
+        sed nulla a lectus molestie pulvinar nec in ante. Morbi imperdiet, nisi
+        sed ultricies aliquam, quam justo venenatis mi, vel finibus est dui
+        egestas odio. Suspendisse et pulvinar augue.
+      </p>
+      <p>
+        Pellentesque ut libero in nisi porta condimentum. Vivamus in pretium
+        velit. Maecenas a pellentesque eros. Etiam ornare pharetra libero at
+        vestibulum. Nam pharetra ullamcorper risus, ac interdum erat tincidunt.
+      </p>
+
+      <h2>Token utility</h2>
+      <img
+        src="https://cpmr-islands.org/wp-content/uploads/sites/4/2019/07/Happy-Test-Screen-01-825x510-600x371.png"
+      />
+      <h3>In-game trading, exclusive access and more</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at nunc
+        tincidunt, volutpat massa fringilla, dapibus urna. Nunc varius commodo
+        luctus. Pellentesque a justo nec orci ornare dictum. Aliquam erat
+        volutpat. Phasellus sed auctor felis.
+      </p>
+
+      <h2>Risks and challenges</h2>
+      <h3>In-game trading, exclusive access and more</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at nunc
+        tincidunt, volutpat massa fringilla, dapibus urna. Nunc varius commodo
+        luctus. Pellentesque a justo nec orci ornare dictum. Aliquam erat
+        volutpat. Phasellus sed auctor felis.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IgniteProjectDescription'
+})
+</script>
+
+<style scoped lang="postcss">
+.project-description {
+  h1,
+  h2,
+  h3 {
+    @apply tracking-tight leading-title;
+  }
+  p,
+  ul {
+    @apply tracking-tighter leading-text;
+  }
+
+  h1,
+  h2 {
+    @apply font-title xl:col-start-2 l:col-span-4;
+  }
+
+  h1 {
+    + video,
+    + img,
+    + iframe {
+      @apply mt-0;
+    }
+  }
+
+  h3,
+  p,
+  ul,
+  video,
+  img,
+  iframe {
+    @apply l:col-start-5 xl:col-start-6 l:col-span-8 xl:col-span-6;
+  }
+
+  h1 {
+    @apply mb-6 m:mb-8 l:mb-0 text-5 m:text-7 font-semibold;
+  }
+  h2 {
+    @apply whitespace-normal text-4 mt-8 m:mt-9 l:mt-9.5 mb-0 font-semibold;
+  }
+  h3 {
+    @apply whitespace-normal text-3 m:text-4 mb-5 mt-6 m:mt-7 l:mt-9.5 mb-0 font-semibold;
+  }
+
+  h3,
+  p,
+  ul {
+    @apply m:px-7;
+  }
+
+  p,
+  ul {
+    @apply text-muted;
+  }
+
+  p + p,
+  h2 + p,
+  h3 + p {
+    @apply mt-5;
+  }
+
+  p + ul,
+  li + li {
+    @apply mt-4;
+  }
+
+  li {
+    @apply relative pl-4;
+    &:before {
+      content: '•';
+      @apply absolute left-0 top-0 text-primary;
+    }
+  }
+
+  video,
+  img,
+  iframe {
+    @apply mb-6 m:mb-7 l:mb-8 w-[100%] rounded mt-7 m:mt-8 l:mt-9.5;
+    + h3 {
+      @apply mt-0;
+    }
+  }
+}
+</style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,24 +2,25 @@
   <div>
     <IgniteProjectHeader />
     <IgniteProjectCards class="mt-8 m:mt-10.5" />
-    <!-- <IgniteProjectAbout class="mt-8 m:mt-10.5" /> -->
+    <IgniteProjectDescription class="mt-8 m:mt-10.5" />
     <!-- <IgniteWhitepaper class="mt-8 m:mt-10.5" /> -->
-    <!-- <IgniteProjectInfo class="mt-8 m:mt-10.5" /> -->
     <!-- <IgniteProjectRoadmap class="mt-8 m:mt-10.5" /> -->
     <!-- <IgniteProjectTokenomics class="mt-8 m:mt-10.5" /> -->
     <!-- <IgniteProjectSchedule class="mt-8 m:mt-10.5" /> -->
     <!-- <IgniteProjectTeam class="mt-8 m:mt-10.5" /> -->
     <!-- <IgniteProjectLinks class="mt-8 m:mt-10.5" /> -->
+    <div class="mt-8 m:mt-10.5"></div>
   </div>
 </template>
 
 <script lang="ts">
+export default {
+  name: 'ProjectView'
+}
+</script>
+
+<script lang="ts" setup>
 import IgniteProjectCards from '../components/IgniteProjectCards/index.vue'
 import IgniteProjectHeader from '../components/IgniteProjectHeader.vue'
-
-export default {
-  name: 'ProjectView',
-
-  components: { IgniteProjectHeader, IgniteProjectCards }
-}
+import IgniteProjectDescription from '../components/IgniteProjectDescription.vue'
 </script>
