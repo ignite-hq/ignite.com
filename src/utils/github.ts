9cbc1f6ee1ff1a74e09216b67a873aac12a36e31
JoseRFelix <jfelx@outlook.com>
2022-04-07 15:38:57 -0400

    fix: display github url in project card

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,10 +1,25 @@
-import { getPathname } from './url'
+export function getUserAndRepositoryFromUrl(githubUrl: string): {
+  githubUser: string
+  githubRepo: string
+  redirectGithubUrl: string
+} {
+  if (!githubUrl) {
+    return { githubUser: '', githubRepo: '', redirectGithubUrl: '' }
+  }
 
-export function getUserAndRepositoryFromUrl(githubUrl: string) {
-  const githubUrlPathname = getPathname(githubUrl)
-  const splitPathname = githubUrlPathname.split('/')
+  const githubUrlIndex = githubUrl.indexOf('github.com')
+  const githubUrlWithoutProtocol = githubUrl.slice(githubUrlIndex)
+  const githubUrlWithProtocol = !githubUrlWithoutProtocol.startsWith('https://')
+    ? `https://${githubUrlWithoutProtocol}`
+    : githubUrlWithoutProtocol
+
+  const splitPathname = githubUrlWithoutProtocol.split('/')
   const githubUser = splitPathname[1] ?? ''
   const githubRepo = splitPathname[2] ?? ''
 
-  return { githubUser, githubRepo }
+  return {
+    githubUser,
+    githubRepo,
+    redirectGithubUrl: githubUrlWithProtocol
+  }
 }
