e60627570a3a825a4a708a9803e40b231a221d29
darika <ppartesia@gmail.com>
2022-04-10 12:53:16 +0400

    update chart

--- src/components/IgniteProjectTokenomics.vue
+++ src/components/IgniteProjectTokenomics.vue
@@ -8,22 +8,7 @@
     </div>
 
     <div class="relative mt-7 m:mt-9">
-      <chart
-        :options="chartOptions"
-        :callback="onRender"
-        :updateArgs="[onRender]"
-      />
-      <div
-        class="absolute overflow-hidden rounded-circle bg-primary scale-[0.65] m:scale-100"
-        :style="{
-          left: `${state.positionLogoX}px`,
-          top: `${state.positionLogoY}px`,
-          width: `${state.logoSize}px`,
-          height: `${state.logoSize}px`
-        }"
-      >
-        <IgniteBgWave />
-      </div>
+      <IgniteDonutChart :colors="colors" :data-series="dataSeries" />
     </div>
   </div>
 </template>
@@ -37,138 +22,47 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-import { Chart } from 'highcharts-vue'
-import IgniteBgWave from './IgniteBgWave.vue'
+import IgniteDonutChart from './IgniteDonutChart.vue'
 
-const state = reactive({
-  positionLogoX: 0,
-  positionLogoY: 0,
-  logoSize: 0
-})
+const colors = [
+  'rgba(9, 78, 253, 1)',
+  'rgba(9, 78, 253, 0.8)',
+  'rgba(9, 78, 253, 0.6)',
+  'rgba(9, 78, 253, 0.4)',
+  'rgba(9, 78, 253, 0.2)',
+  'rgba(9, 78, 253, 0.1)'
+]
 
-const onRender = (chart) => {
-  state.logoSize = chart.plotHeight * 0.575
-  state.positionLogoX =
-    chart.plotLeft + chart.series[0].center[0] - state.logoSize / 2
-  state.positionLogoY =
-    chart.plotTop + chart.series[0].center[1] - state.logoSize / 2
-}
-
-const chartOptions = {
-  credits: {
-    enabled: false
+const dataSeries = [
+  {
+    name: 'Marketing',
+    y: 5
   },
-  chart: {
-    type: 'pie',
-    height: '545px',
-    events: {
-      redraw: function () {
-        onRender(this)
-      }
+  {
+    name: 'Fundraiser',
+    y: 10
+  },
+  {
+    name: 'Airdrop',
+    y: 25,
+    description: 'Note or description lorem ipsum dolor sit amet.',
+    dataLabels: {
+      enabled: true
     }
   },
-  title: false,
-  tooltip: {
-    headerFormat: '',
-    pointFormat:
-      '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b>: {point.y}%'
+  {
+    name: 'Community Pool',
+    y: 35
   },
-  plotOptions: {
-    series: {
-      dataLabels: {
-        enabled: false,
-        shape: 'callout',
-        formatter: function () {
-          return `
-            <span style="padding:8px;font-size:13px;font-weight:500;">${
-              Math.round(this.percentage * 100) / 100
-            }%</span>
-            <span style="font-size:16px;font-weight:600;">${this.key}</span>
-            <br/>
-            <text dy="4" style="color:rgba(0, 0, 0, 0.667);font-size:13px;font-weight:400;">${
-              this.point.description
-            }</text>
-          `
-        }
-      }
-    },
-    pie: {
-      allowPointSelect: false,
-      showInLegend: true,
-      point: {
-        events: {
-          legendItemClick: () => false
-        }
-      }
-    }
+  {
+    name: 'Public Sale',
+    y: 15
   },
-  legend: {
-    margin: 34,
-    itemDistance: 48,
-    itemMarginTop: 16,
-    labelFormatter: function () {
-      return `
-        <span style="font-size:16px;font-weight:600;">${this.name}</span><br/>
-        <span dy="4" style="color:#626262;font-size:13px;font-weight:500;">${
-          Math.round(this.percentage * 100) / 100
-        }%<span>
-      `
-    }
-  },
-  colors: [
-    'rgba(9, 78, 253, 1)',
-    'rgba(9, 78, 253, 0.8)',
-    'rgba(9, 78, 253, 0.6)',
-    'rgba(9, 78, 253, 0.4)',
-    'rgba(9, 78, 253, 0.2)',
-    'rgba(9, 78, 253, 0.1)'
-  ],
-  series: [
-    {
-      minPointSize: 10,
-      innerSize: '85%',
-      zMin: 0,
-      name: 'countries',
-      data: [
-        {
-          name: 'Marketing',
-          y: 5
-        },
-        {
-          name: 'Fundraiser',
-          y: 10
-        },
-        {
-          name: 'Airdrop',
-          y: 25,
-          description: 'Note or description lorem ipsum dolor sit amet.',
-          dataLabels: {
-            enabled: true
-          }
-        },
-        {
-          name: 'Community Pool',
-          y: 35
-        },
-        {
-          name: 'Public Sale',
-          y: 15
-        },
-        {
-          name: 'Core Team',
-          y: 10
-        }
-      ]
-    }
-  ]
-}
+  {
+    name: 'Core Team',
+    y: 10
+  }
+]
 </script>
 
-<script setup lang="ts"></script>
-
-<style lang="postcss">
-svg.highcharts-root {
-  font-family: 'Inter', sans-serif !important;
-}
-</style>
+<style lang="postcss"></style>
