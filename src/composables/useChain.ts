8d1547e8994c5342d9aee526ee2a37699ab3f0cd
Matthew Salamon <35425388+Matthews3301@users.noreply.github.com>
2022-04-12 15:57:26 +0100

    Update src/composables/useChain.ts
    Co-authored-by: Arthur Marinho <arthurtutuca@gmail.com>

--- src/composables/useChain.ts
+++ src/composables/useChain.ts
@@ -3,7 +3,6 @@ import { computed, ref } from 'vue'
 import { useQuery } from 'vue-query'
 
 export default function useChain(launchID: ref<string>) {
-  const { ignite } = useIgnite()
   const { queryChain } = useTendermintSpnLaunch()
 
   const { data, ...other } = useQuery(['launches', launchID], () => {
