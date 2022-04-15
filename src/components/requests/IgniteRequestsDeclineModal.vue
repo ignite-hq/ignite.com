512e26060ca7d0932cc6d7af8c960f0f012a1920
Jose Felix <jfelx@outlook.com>
2022-04-14 22:01:16 -0400

    feat: add decline request modal transaction

--- src/components/requests/IgniteRequestsDeclineModal.vue
+++ src/components/requests/IgniteRequestsDeclineModal.vue
@@ -22,6 +22,7 @@
           color="primary"
           type="submit"
           class="flex-1"
+          @click="onConfirm"
         >
           Confirm
         </IgniteButton>
@@ -37,14 +38,62 @@ export default {
 </script>
 
 <script lang="ts" setup>
+import { EncodeObject } from '@cosmjs/proto-signing'
+import { useIgnite as useIgniteN } from '@ignt/vue'
+import { useIgnite } from 'tendermint-spn-vue'
+
 import IconWarning from '~/components/icons/IconWarning.vue'
 import IgniteButton from '~/components/IgniteButton.vue'
 import IgniteHeading from '~/components/IgniteHeading.vue'
 import IgniteModal from '~/components/IgniteModal.vue'
+import { useRequestsStore } from '~/stores/requests-store'
 
 interface Emits {
   (e: 'close'): void
 }
 
 defineEmits<Emits>()
+
+const store = useRequestsStore()
+const { ignite } = useIgnite()
+const {
+  state: { ignite: igniteN }
+} = useIgniteN()
+
+// menthods
+
+function getTransactionMessages(signer: string) {
+  const requests = store.selectedRequests
+  const messages: EncodeObject[] = []
+
+  requests.forEach(({ launchID, content, requestID }) => {
+    if (!content) return
+
+    const message = ignite.tendermintSpnLaunch.value.msgSettleRequest({
+      value: {
+        approve: false,
+        launchID: Number(launchID),
+        requestID: Number(requestID),
+        signer: signer
+      }
+    })
+
+    messages.push(message)
+  })
+
+  return messages
+}
+
+function onConfirm() {
+  const signerAddress = igniteN.value.addr
+
+  if (!signerAddress) return
+
+  const messages = getTransactionMessages(signerAddress)
+
+  igniteN.value.signer?.signAndBroadcast(signerAddress, messages, {
+    amount: [],
+    gas: '200000'
+  })
+}
 </script>
