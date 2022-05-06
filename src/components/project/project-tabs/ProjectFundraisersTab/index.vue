eed28319b2c1063d3180d5c94937f0d9b3f155a7
Matthews <salamon.mat@gmail.com>
2022-05-06 08:53:45 +0100

    formatAuctionStatus now switch

--- src/components/project/project-tabs/ProjectFundraisersTab/index.vue
+++ src/components/project/project-tabs/ProjectFundraisersTab/index.vue
@@ -14,7 +14,10 @@ import InvestVoucherAllocation from './InvestVoucherAllocation.vue'
 import { computed, watchEffect, ref } from 'vue'
 import BigNumber from 'bignumber.js'
 
-import { FixedPriceAuction } from '~/generated/tendermint-spn-ts-client/tendermint.fundraising'
+import {
+  BaseAuction,
+  FixedPriceAuction
+} from '~/generated/tendermint-spn-ts-client/tendermint.fundraising'
 import { AuctionStatus } from '~/generated/tendermint-spn-ts-client/tendermint.fundraising/types/fundraising/fundraising'
 import useFundraisersAll from '~/composables/fundraising/useFundraisersAll'
 import { useCosmosBankV1Beta1 } from '~/generated/tendermint-spn-vue'
@@ -32,39 +35,59 @@ withDefaults(defineProps<Props>(), {
   campaignSummary: () => ({})
 })
 
-const formatAuctionStatus = (auctionType: AuctionStatus): string => {
-  if (auctionType == 'AUCTION_STATUS_VESTING') return 'Current'
-  if (auctionType == 'AUCTION_STATUS_STARTED') return 'Current'
-  if (auctionType == 'AUCTION_STATUS_STANDBY') return 'Upcoming'
-  if (auctionType == 'AUCTION_STATUS_FINISHED') return 'Previous'
-  if (auctionType == 'AUCTION_STATUS_CANCELLED') return 'Previous'
-  if (auctionType == 'AUCTION_STATUS_UNSPECIFIED') return 'Other'
-  return 'Other'
+enum AuctionHeadingStatus {
+  CurrentAndUpcoming = 'Current and upcoming',
+  Current = 'Current',
+  Upcoming = 'Upcoming',
+  Previous = 'Previous',
+  Other = 'Other'
+}
+
+const formatAuctionStatus = (
+  auctionType: AuctionStatus
+): AuctionHeadingStatus => {
+  switch (AuctionStatus[auctionType] as AuctionStatus) {
+    case AuctionStatus.AUCTION_STATUS_VESTING:
+    case AuctionStatus.AUCTION_STATUS_STARTED:
+      return AuctionHeadingStatus.Current
+    case AuctionStatus.AUCTION_STATUS_STANDBY:
+      return AuctionHeadingStatus.Upcoming
+    case AuctionStatus.AUCTION_STATUS_FINISHED:
+    case AuctionStatus.AUCTION_STATUS_CANCELLED:
+      return AuctionHeadingStatus.Previous
+    default:
+      return AuctionHeadingStatus.Other
+  }
 }
 
 const statuses: string[] = computed(() => {
   let statuses = []
   const currentPresent = fundraisers?.value?.pages[0].auctions.find(
     (auctionData: FixedPriceAuction) =>
-      formatAuctionStatus(auctionData.base_auction.status) === 'Current'
+      formatAuctionStatus(auctionData.base_auction.status) ===
+      AuctionHeadingStatus.Current
   )
   const upcomingPresent = fundraisers?.value?.pages[0].auctions.find(
     (auctionData: FixedPriceAuction) =>
-      formatAuctionStatus(auctionData.base_auction.status) === 'Upcoming'
+      formatAuctionStatus(auctionData.base_auction.status) ===
+      AuctionHeadingStatus.Upcoming
   )
   const previousPresent = fundraisers?.value?.pages[0].auctions.find(
     (auctionData: FixedPriceAuction) =>
-      formatAuctionStatus(auctionData.base_auction.status) === 'Previous'
+      formatAuctionStatus(auctionData.base_auction.status) ===
+      AuctionHeadingStatus.Previous
   )
   const otherPresent = fundraisers?.value?.pages[0].auctions.find(
     (auctionData: FixedPriceAuction) =>
-      formatAuctionStatus(auctionData.base_auction.status) === 'Other'
+      formatAuctionStatus(auctionData.base_auction.status) ===
+      AuctionHeadingStatus.Other
   )
-  if (currentPresent && upcomingPresent) statuses.push('Current and upcoming')
-  else if (currentPresent) statuses.push('Current')
-  else if (upcomingPresent) statuses.push('Upcoming')
-  if (previousPresent) statuses.push('Previous')
-  if (otherPresent) statuses.push('Other')
+  if (currentPresent && upcomingPresent)
+    statuses.push(AuctionHeadingStatus.CurrentAndUpcoming)
+  else if (currentPresent) statuses.push(AuctionHeadingStatus.Current)
+  else if (upcomingPresent) statuses.push(AuctionHeadingStatus.Upcoming)
+  if (previousPresent) statuses.push(AuctionHeadingStatus.Previous)
+  if (otherPresent) statuses.push(AuctionHeadingStatus.Other)
 
   return statuses
 })
@@ -77,7 +100,7 @@ watchEffect(async () => {
 const fundraisingList = computed(() => {
   return fundraisers?.value?.pages[0].auctions.map(
     (auctionData: FixedPriceAuction) => {
-      const auction = auctionData.base_auction
+      const auction: BaseAuction = auctionData.base_auction
       const token = totalSupply.value?.find(
         (token) => token.denom === auction.selling_coin.denom
       )
@@ -119,6 +142,7 @@ const fundraisingList = computed(() => {
       class="mt-8 md:mt-10.5"
     />
     <div class="container-full container px-5 sm:px-5.5 lg:px-7">
+      {{ statuses }}
       <div v-for="status in statuses" :key="status" class="mt-8 md:mt-10.5">
         <IgniteHeading
           as="div"
@@ -131,7 +155,10 @@ const fundraisingList = computed(() => {
         >
           <div
             v-for="(item, key) in fundraisingList?.filter((fundraiser) =>
-              status.toLowerCase().includes(fundraiser.status.toLowerCase())
+              status
+                .toString()
+                .toLowerCase()
+                .includes(fundraiser.status.toString().toLowerCase())
             )"
             :key="`fundraisers_${status}_${key}`"
             class="relative z-[1]"
