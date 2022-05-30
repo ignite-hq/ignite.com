d0b7bee9eef63cebeb4c42f069d824886cb7c073
Matthew Salamon <35425388+Matthews3301@users.noreply.github.com>
2022-05-30 19:24:30 +0100

    fix: Show project fundraisers only (#125)
    * fix: Only show fundraisers for the selected project

--- src/components/project/project-tabs/ProjectFundraisersTab/index.vue
+++ src/components/project/project-tabs/ProjectFundraisersTab/index.vue
@@ -11,9 +11,12 @@ import { computed, ref, watchEffect } from 'vue'
 import IgniteCard from '~/components/ui/IgniteCard.vue'
 import IgniteHeading from '~/components/ui/IgniteHeading.vue'
 import IgniteText from '~/components/ui/IgniteText.vue'
-import useFundraisersAll from '~/composables/fundraising/useFundraisersAll'
+import useCampaignFundraisers from '~/composables/fundraising/useCampaignFundraisers'
 import { V1Beta1Coin } from '~/generated/tendermint-spn-ts-client/cosmos.bank.v1beta1/rest'
-import { BaseAuction } from '~/generated/tendermint-spn-ts-client/tendermint.fundraising'
+import {
+  BaseAuction,
+  FixedPriceAuction
+} from '~/generated/tendermint-spn-ts-client/tendermint.fundraising'
 import { Coin } from '~/generated/tendermint-spn-ts-client/tendermint.fundraising/types/cosmos/base/v1beta1/coin'
 import { AuctionStatus } from '~/generated/tendermint-spn-ts-client/tendermint.fundraising/types/fundraising/fundraising'
 import { CampaignCampaignSummary } from '~/generated/tendermint-spn-ts-client/tendermint.spn.campaign/rest'
@@ -25,17 +28,28 @@ import InvestCard from './InvestCard.vue'
 import InvestStart from './InvestStart.vue'
 import InvestTitle from './InvestTitle.vue'
 import InvestVoucherAllocation from './InvestVoucherAllocation.vue'
-
-const { fundraisers } = useFundraisersAll()
-const { queryTotalSupply } = useCosmosBankV1Beta1()
+import { useRoute } from 'vue-router'
+import useCoordinator from '~/composables/profile/useCoordinator'
 
 interface Props {
   campaignSummary?: CampaignCampaignSummary
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   campaignSummary: () => ({})
 })
+
+const route = useRoute()
+const projectId = route.params.projectId.toString() || '0'
+
+const coordinatorId = computed(() => {
+  return props.campaignSummary?.campaign?.coordinatorID
+})
+
+const { fundraisers } = useCampaignFundraisers(projectId)
+const { queryTotalSupply } = useCosmosBankV1Beta1()
+const { isSameAddressAsLoggedIn: coordinatorView } =
+  useCoordinator(coordinatorId)
 
 const formatAuctionStatus = (
   auctionType: AuctionStatus
@@ -54,28 +68,28 @@ const formatAuctionStatus = (
       return AuctionStatusLabels.Other
   }
 }
-
+// computed
 const statuses = computed(() => {
   let statuses = []
-  const auctions = fundraisers?.value?.pages[0].auctions ?? []
+  const auctions = fundraisers?.value ?? []
   const currentPresent = auctions.find(
-    (auctionData: any) =>
-      formatAuctionStatus(auctionData.base_auction.status) ===
+    ({ auction }: FixedPriceAuction) =>
+      formatAuctionStatus(auction.base_auction.status) ===
       AuctionStatusLabels.Current
   )
   const upcomingPresent = auctions.find(
-    (auctionData: any) =>
-      formatAuctionStatus(auctionData.base_auction.status) ===
+    ({ auction }: FixedPriceAuction) =>
+      formatAuctionStatus(auction.base_auction.status) ===
       AuctionStatusLabels.Upcoming
   )
   const previousPresent = auctions.find(
-    (auctionData: any) =>
-      formatAuctionStatus(auctionData.base_auction.status) ===
+    ({ auction }: FixedPriceAuction) =>
+      formatAuctionStatus(auction.base_auction.status) ===
       AuctionStatusLabels.Previous
   )
   const otherPresent = auctions.find(
-    (auctionData: any) =>
-      formatAuctionStatus(auctionData.base_auction.status) ===
+    ({ auction }: FixedPriceAuction) =>
+      formatAuctionStatus(auction.base_auction.status) ===
       AuctionStatusLabels.Other
   )
   if (currentPresent && upcomingPresent)
@@ -94,35 +108,33 @@ watchEffect(async () => {
 })
 
 const fundraisingList = computed(() => {
-  return (fundraisers?.value?.pages[0].auctions || []).map(
-    (auctionData: any) => {
-      const auction: BaseAuction = auctionData.base_auction
-      const remainingSellingCoins: Coin | undefined =
-        auctionData.remaining_selling_coin
-      const token = totalSupply.value?.find(
-        (token) => token.denom === auction.selling_coin?.denom
-      )
-      const tokenSupply = new BigNumber(token?.amount ?? '0').toNumber()
-      const relativeSupply = tokenSupply
-        ? Math.round(
-            (Number(auction.selling_coin?.amount ?? 0) / tokenSupply) * 100
-          )
-        : 0
-      return {
-        id: auction.id,
-        raised:
-          Number(auction.selling_coin?.amount ?? 0) -
-          Number(remainingSellingCoins?.amount ?? 0),
-        goal: auction.selling_coin?.amount ?? 0,
-        currency: getDenomName(auction.selling_coin?.denom ?? ''),
-        status: formatAuctionStatus(auction.status),
-        statusDetailed: auction.status,
-        vouchers: `${toCompactNumber.format(tokenSupply)} (${relativeSupply}%)`,
-        investors: auction.allowed_bidders?.length || 0,
-        ends: auction.end_times[0]
-      } as AuctionCardData
-    }
-  )
+  return (fundraisers?.value || []).map(({ auction }: FixedPriceAuction) => {
+    const baseAuction: BaseAuction = auction.base_auction
+    const remainingSellingCoins: Coin | undefined =
+      auction.remaining_selling_coin
+    const token = totalSupply.value?.find(
+      (token) => token.denom === baseAuction.selling_coin?.denom
+    )
+    const tokenSupply = new BigNumber(token?.amount ?? '0').toNumber()
+    const relativeSupply = tokenSupply
+      ? Math.round(
+          (Number(baseAuction.selling_coin?.amount ?? 0) / tokenSupply) * 100
+        )
+      : 0
+    return {
+      id: baseAuction.id,
+      raised:
+        Number(baseAuction.selling_coin?.amount ?? 0) -
+        Number(remainingSellingCoins?.amount ?? 0),
+      goal: baseAuction.selling_coin?.amount ?? 0,
+      currency: getDenomName(baseAuction.selling_coin?.denom ?? ''),
+      status: formatAuctionStatus(baseAuction.status),
+      statusDetailed: baseAuction.status,
+      vouchers: `${toCompactNumber.format(tokenSupply)} (${relativeSupply}%)`,
+      investors: auction.allowed_bidders?.length || 0,
+      ends: baseAuction.end_times[0]
+    } as AuctionCardData
+  })
 })
 </script>
 
@@ -130,6 +142,7 @@ const fundraisingList = computed(() => {
   <div>
     <InvestTitle
       :project-name="campaignSummary?.campaign?.campaignName"
+      :allow-create="coordinatorView"
       class="mt-8 md:mt-10.5"
     />
     <InvestVoucherAllocation
@@ -138,6 +151,7 @@ const fundraisingList = computed(() => {
       class="mt-7 md:mt-9"
     />
     <InvestStart
+      v-if="coordinatorView && fundraisers?.length === 0"
       :project-name="campaignSummary?.campaign?.campaignName"
       class="mt-8 md:mt-10.5"
     />
@@ -167,7 +181,9 @@ const fundraisingList = computed(() => {
         </div>
       </div>
     </div>
-
+    <div v-if="fundraisers?.length === 0" class="text-center">
+      <span>- No fundraisers yet -</span>
+    </div>
     <div class="z-1 container relative mt-8 md:mt-10.5">
       <IgniteCard
         class="flex flex-col p-6 md:flex-row md:items-center md:justify-between md:gap-7 md:p-7.5 lg:p-9 xl:pr-11"
