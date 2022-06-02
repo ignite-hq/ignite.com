c6c4eba887f4dda7f813e3a70ea066ba69780098
darika <ppartesia@gmail.com>
2022-06-02 19:36:51 +0400

    add mobile styles

--- src/components/validate/validate-tabs/ValidateProjectsTab/ProjectCard.vue
+++ src/components/validate/validate-tabs/ValidateProjectsTab/ProjectCard.vue
@@ -26,9 +26,9 @@ defineProps({
 
 <template>
   <IgniteLink to="/projects" class="mb-7 w-full last:mb-0">
-    <IgniteCard :shadow="true" class="flex w-full flex-col md:flex-row">
+    <IgniteCard :shadow="true" class="flex w-full flex-col lg:flex-row">
       <div
-        class="flex shrink-0 flex-col items-center justify-center px-7.5 py-8 md:w-[23.875rem]"
+        class="flex shrink-0 flex-col items-center justify-center px-5 py-7 md:px-9 md:py-8 lg:w-[15rem] lg:px-6 lg:py-8 xl:w-[23.875rem] xl:px-7.5 xl:py-8"
       >
         <div class="relative h-9.5 w-9.5 shrink-0 rounded-md bg-primary">
           <div class="absolute inset-0 z-[2] overflow-hidden">
@@ -36,7 +36,7 @@ defineProps({
           </div>
         </div>
 
-        <IgniteHeading as="div" class="mt-5 font-title text-5">{{
+        <IgniteHeading as="div" class="mt-5 text-center font-title text-5">{{
           project.name
         }}</IgniteHeading>
         <IgniteText
@@ -56,7 +56,9 @@ defineProps({
         </IgniteText>
       </div>
 
-      <div class="border-x border-border px-7.5 py-8 md:w-full">
+      <div
+        class="border-y border-border px-5 py-7 md:w-full md:px-9 md:py-8 lg:border-y-0 lg:border-x lg:px-6 lg:py-8 xl:px-7.5 xl:py-8"
+      >
         <IgniteText as="div" class="font-semibold">{{
           project.testnet.name
         }}</IgniteText>
@@ -75,28 +77,30 @@ defineProps({
         <IgniteText
           v-if="project.testnet.rewards"
           as="div"
-          class="mt-5 flex items-center text-2"
+          class="mt-5 flex flex-col text-2 xl:flex-row xl:items-center"
         >
-          <span class="mr-3">Rewards total:</span>
-          <IgniteText
-            v-for="reward in project.testnet.rewards"
-            :key="reward"
-            as="div"
-            class="mr-5 flex items-center last:mr-0"
-          >
-            <IgniteDenom
-              v-if="reward.denom"
-              modifier="avatar"
-              :denom="reward.denom"
-              :title="reward.denom"
-              size="small"
-              class="mr-3"
-            />
-            {{ reward.count }}
-          </IgniteText>
+          <span class="mb-3 xl:mb-0 xl:mr-3">Rewards total:</span>
+          <div class="flex w-full items-center">
+            <IgniteText
+              v-for="reward in project.testnet.rewards"
+              :key="reward"
+              as="div"
+              class="mr-5 flex items-center last:mr-0"
+            >
+              <IgniteDenom
+                v-if="reward.denom"
+                modifier="avatar"
+                :denom="reward.denom"
+                :title="reward.denom"
+                size="small"
+                class="mr-3"
+              />
+              {{ reward.count }}
+            </IgniteText>
+          </div>
         </IgniteText>
 
-        <div v-if="project.fundraisers" class="mt-7.5 flex justify-between">
+        <div v-if="project.fundraisers" class="mt-7.5 justify-between sm:flex">
           <div>
             <IgniteText as="div" class="font-semibold">Fundraisers</IgniteText>
             <IgniteText as="div" class="mt-1 text-2 text-muted"
@@ -104,7 +108,7 @@ defineProps({
               {{ project.fundraisers.upcoming }} upcoming</IgniteText
             >
           </div>
-          <div>
+          <div class="mt-4 sm:mt-0">
             <IgniteText as="div" class="flex items-center">
               <IgniteDenom
                 modifier="avatar"
@@ -120,7 +124,9 @@ defineProps({
         </div>
       </div>
 
-      <div class="px-7.5 py-8 md:w-full">
+      <div
+        class="px-5 py-7 md:w-full md:px-9 md:py-8 lg:px-6 lg:py-8 xl:px-7.5 xl:py-8"
+      >
         <IgniteText as="div" class="font-semibold"
           >Mainnet token distribution</IgniteText
         >
@@ -133,9 +139,9 @@ defineProps({
         />
         <IgniteLegend :items="project.distribution.legend" class="mt-5" />
 
-        <div class="mt-7.5 flex justify-between">
+        <div class="mt-7.5 justify-between sm:flex">
           <IgniteText as="div" class="font-semibold">Your vouchers:</IgniteText>
-          <div>
+          <div class="mt-4 sm:mt-0">
             <div class="flex items-start">
               <IgniteDenom
                 modifier="avatar"
@@ -156,106 +162,6 @@ defineProps({
           </div>
         </div>
       </div>
-
-      <!-- <IgniteProgressBar :items="progressBar.items" :label="false" size="xs" />
-      <IgniteHeading
-        as="div"
-        class="mt-5 font-title text-4 font-semibold md:text-5"
-      >
-        <IgniteNumber :number="Number(data.raised)" />
-        {{ data.currency }}
-      </IgniteHeading>
-      <IgniteHeading as="div" class="mt-3 text-3 text-muted">
-        Raised of
-        <strong>
-          <IgniteNumber :number="Number(data.goal)" /> {{ data.currency }}
-        </strong>
-      </IgniteHeading>
-      <div
-        class="mt-7.5 grid grid-cols-2 gap-6 border-t border-border pt-6 sm:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4 xl:pt-7.5"
-      >
-        <div class="">
-          <IgniteText as="div" class="text-2 text-muted"> Status </IgniteText>
-          <IgniteHeading
-            as="div"
-            class="mt-2 flex items-center text-2 font-semibold md:mt-3 md:text-3"
-          >
-            <IconDots
-              v-if="data.statusDetailed === 'AUCTION_STATUS_VESTING'"
-              class="mr-3"
-            />
-            <IconDots
-              v-if="data.statusDetailed === 'AUCTION_STATUS_STARTED'"
-              class="mr-3"
-            />
-            <IconClock
-              v-if="data.statusDetailed === 'AUCTION_STATUS_STANDBY'"
-              class="mr-3"
-            />
-            <IconCheckMarkThin
-              v-if="data.statusDetailed === 'AUCTION_STATUS_FINISHED'"
-              class="mr-3"
-            />
-            <IconCanceled
-              v-if="data.statusDetailed === 'AUCTION_STATUS_CANCELLED'"
-              class="mr-3"
-            />
-            <IconCanceled
-              v-if="data.statusDetailed === 'AUCTION_STATUS_UNSPECIFIED'"
-              class="mr-3"
-            />
-            {{ data.status }}
-          </IgniteHeading>
-        </div>
-
-        <div class="">
-          <IgniteText as="div" class="text-2 text-muted"> Vouchers </IgniteText>
-          <IgniteHeading
-            as="div"
-            class="mt-2 flex items-center text-2 font-semibold md:mt-3 md:text-3"
-          >
-            <IconStack class="mr-3" />
-            {{ data.vouchers }}
-          </IgniteHeading>
-        </div>
-
-        <div class="">
-          <IgniteText as="div" class="text-2 text-muted">
-            {{
-              data.status === AuctionStatusLabels.Upcoming
-                ? 'Registrants'
-                : 'Participants'
-            }}
-          </IgniteText>
-          <IgniteHeading
-            as="div"
-            class="mt-2 flex items-center text-2 font-semibold md:mt-3 md:text-3"
-          >
-            <IconUser class="mr-3" />
-            <IgniteNumber :number="Number(data.investors)" />
-          </IgniteHeading>
-        </div>
-
-        <div class="">
-          <IgniteText as="div" class="text-2 text-muted">
-            {{ Date.now() < new Date(data.ends).getTime() ? 'Ends' : 'Ended' }}
-          </IgniteText>
-          <IgniteHeading
-            as="div"
-            class="mt-2 flex items-center text-2 font-semibold md:mt-3 md:text-3"
-          >
-            <IconCalendar class="mr-3" />
-            <span
-              :class="
-                data.statusDetailed === 'AUCTION_STATUS_CANCELLED' &&
-                'text-inactive line-through'
-              "
-            >
-              {{ new Date(data.ends).toLocaleDateString() }}
-            </span>
-          </IgniteHeading>
-        </div>
-      </div> -->
     </IgniteCard>
   </IgniteLink>
 </template>
